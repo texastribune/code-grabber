185eb292f053b83b64cc89eccb345bd8cba8c2b2
becca <brickaa@users.noreply.github.com>
2016-05-12 12:18:53 -0500

    restyle tooltip, refactor copied!, refactor series header blocks

--- app/scripts/includes/copy.js
+++ app/scripts/includes/copy.js
@@ -8,15 +8,20 @@ new Clipboard('.copy');
 
 // Displays nearest "Copied!" checkmark
 // to provide user feedback ON FORM SUBMIT
-
 export function copied(x) {
   $('.copied').hide();
   $(x).find('.copied').css('display', 'inline-block');
+
+  console.log('copied');
 }
 
 // Displays nearest "Copied!" checkmark
 // to provide user feedback ON NON-FORM COMPONENTS
-$('button.copy-button').click(function() {
-  $('.copied').hide();
-  $(this).next($('.copied')).css('display', 'inline-block');
+$('.copy--button').click(function() {
+  var parent = $(this).closest('.copy--wrapper'),
+      tooltip = parent.find('.copy--tooltip');
+
+  $('.copy--tooltip').hide();
+  tooltip.css('display', 'inline-block');
+  console.log(tooltip);
 });
