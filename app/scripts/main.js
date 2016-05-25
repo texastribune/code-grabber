9c5dd8f24b3cacef38e7bc0d0d4cd83f8d38fdbf
Kathryn Beaty <kabeaty@gmail.com>
2016-05-25 12:41:24 -0500

    Make readmore links asides too

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,7 +8,7 @@ import Clipboard from 'clipboard';
 new Clipboard('.copy');
 
 function readmore(headlineSlug, link, headline) {
-  var codeBlock = '<p class="readmore" style="font-style: italic; padding-top: .5em; padding-bottom: .5em; vertically-align: middle;"><span class="readmore--label" style="color: #111111; font-family: Helvetica,Arial,sans-serif; font-size: .9em; font-style: italic; font-weight: 800; margin: 0 1em 1em 0; text-decoration: none; text-transform: uppercase;">Read More</span><a onclick="ga(\'send\', \'event\', \'codegrabber\', \'click\', \'readmore\', \'' + headlineSlug + '\', {\'nonInteraction\': 1})" class="readmore_link" href="'+ link +'">'+ headline +'</a></p>';
+  var codeBlock = '<aside class="readmore" style="font-style: italic; margin-bottom: 1em; padding-top: .5em; padding-bottom: .5em; vertically-align: middle;"><span class="readmore--label" style="color: #111111; font-family: Helvetica,Arial,sans-serif; font-size: .9em; font-style: italic; font-weight: 800; margin: 0 1em 1em 0; text-decoration: none; text-transform: uppercase;">Read More</span><a onclick="ga(\'send\', \'event\', \'codegrabber\', \'click\', \'readmore\', \'' + headlineSlug + '\', {\'nonInteraction\': 1})" class="readmore_link" href="'+ link +'">'+ headline +'</a></aside>';
 
   return codeBlock;
 }
