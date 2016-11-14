a34d13ce3268c0cdbe3b219af3c0593c122b47e1
Daniel Craigmile <dcraigmile@texastribune.org>
2016-11-14 12:53:49 -0600

    switch it to not use a folder

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
 echo "S3 BUCKET: "${npm_package_config_s3_bucket:?"The S3 bucket needs to be set in the package.json"}
-echo "PROJECT SLUG: "${npm_package_config_slug:?"The project's slug needs to be set in the package.json"}
+#echo "PROJECT SLUG: "${npm_package_config_slug:?"The project's slug needs to be set in the package.json"}
 echo
 
 export APP_S3_BUCKET=$npm_package_config_s3_bucket
-export PROJECT_SLUG=$npm_package_config_slug
+#export PROJECT_SLUG=$npm_package_config_slug
 
 echo "Syncing *.css files to S3..."
-aws s3 sync --acl public-read --profile newsapps --exclude '*.*' --include '*.css' --cache-control 'max-age=31536000' dist s3://$APP_S3_BUCKET/$PROJECT_SLUG/
+aws s3 sync --acl public-read --profile newsapps --exclude '*.*' --include '*.css' --cache-control 'max-age=31536000' dist s3://$APP_S3_BUCKET/
 
 echo "Syncing *.js files to S3..."
-aws s3 sync --acl public-read --profile newsapps --exclude '*.*' --include '*.js' --cache-control 'max-age=31536000' dist s3://$APP_S3_BUCKET/$PROJECT_SLUG/
+aws s3 sync --acl public-read --profile newsapps --exclude '*.*' --include '*.js' --cache-control 'max-age=31536000' dist s3://$APP_S3_BUCKET/
 
 echo "Syncing *.html files to S3..."
-aws s3 sync --acl public-read --profile newsapps --exclude '*.*' --include '*.html' dist s3://$APP_S3_BUCKET/$PROJECT_SLUG/
+aws s3 sync --acl public-read --profile newsapps --exclude '*.*' --include '*.html' --cache-control 'max-age=60' dist s3://$APP_S3_BUCKET/
 
 echo "Syncing image files to S3..."
-aws s3 sync --acl public-read --profile newsapps --exclude '*.*' --include '*.jpg' --include '*.png' --include '*.gif' --cache-control 'max-age=604800' dist s3://$APP_S3_BUCKET/$PROJECT_SLUG/
+aws s3 sync --acl public-read --profile newsapps --exclude '*.*' --include '*.jpg' --include '*.png' --include '*.gif' --cache-control 'max-age=604800' dist s3://$APP_S3_BUCKET/
 
 echo "Syncing *.svg files to S3..."
-aws s3 sync --acl public-read --profile newsapps --exclude '*.*' --include '*.svg' --cache-control 'max-age=604800' dist s3://$APP_S3_BUCKET/$PROJECT_SLUG/
+aws s3 sync --acl public-read --profile newsapps --exclude '*.*' --include '*.svg' --cache-control 'max-age=604800' dist s3://$APP_S3_BUCKET/
 
 echo "Syncing *.json files to S3..."
-aws s3 sync --acl public-read --profile newsapps --exclude '*.*' --include '*.json' --cache-control 'max-age=3600' dist s3://$APP_S3_BUCKET/$PROJECT_SLUG/
+aws s3 sync --acl public-read --profile newsapps --exclude '*.*' --include '*.json' --cache-control 'max-age=3600' dist s3://$APP_S3_BUCKET/
 
 echo "Syncing everything else to S3..."
-aws s3 sync --profile newsapps dist s3://$APP_S3_BUCKET/$PROJECT_SLUG/
+aws s3 sync --profile newsapps dist s3://$APP_S3_BUCKET/
